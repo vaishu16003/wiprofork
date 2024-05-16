4f42b48f272f3519d29958186362502e3db54780
Deeps <deepikkaa20@gmail.com>
2024-05-16 17:07:55 +0530

    add() added

--- File1.java
+++ File1.java
@@ -4,6 +4,10 @@ public class File1
 {
 	int x=1000;
 	int y=200;
+
+	public void add(){
+	     System.out.println(x+y);
+	}
 	public static void main(String[] args){
 	 System.out.println("First Java");
 	 System.out.println("My Second Line of Code"); 		
