f97c3ac7354ff6863590e5a47f654bc175dd3012
Deeps <50661604+chntraining@users.noreply.github.com>
2024-05-16 14:38:12 +0530

    Update File1.java variable x added

--- File1.java
+++ File1.java
@@ -2,8 +2,9 @@ import java.lang.*;
 
 public class File1
 {
+	int x=1000;
 	public static void main(String[] args){
 	 System.out.println("First Java");
-	 System.out.println("My Second Line of Code"); 
+	 System.out.println("My Second Line of Code"); 		
 	}
 }
